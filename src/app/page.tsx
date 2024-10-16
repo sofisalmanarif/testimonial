e3bcb707fd80193ec543612885ace8be56f7a57b
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-16 22:21:24 +0530

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
-import { Button } from "@/components/ui/button";
-import ShimmerButton from "@/components/ui/ShimmerButton";
-import Image from "next/image";
 
-export default function Home() {
+import { ArrowBigLeft, ArrowBigRight, ArrowRight } from "lucide-react";
+import React from "react";
+
+ function Home() {
   return (
-    <div>
-      <h1>Transform Feedback into Trust: Discover How Your Customers' Stories Can Elevate Your Brand and Drive Engagement!</h1>
+    <div className="h-[40rem] w-full rounded-md flex flex-col justify-center md:items-center md:justify-center  antialiased bg-grid-white/[0.02] relative overflow-hidden">
+     
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        Unlock the Power of Social Proof: <br /> Capture Testimonials That Convert!
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          Spotlight effect is a great way to draw attention to a specific part
+          of the page. Here, we are drawing the attention towards the text
+          section of the page. I don&apos;t know why but I&apos;m running out of
+          copy.
+        </p>
+
+      
+      </div>
+      <button className="relative inline-flex mt-10 h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <span className="absolute inset-[-1000%]  animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full px-6 cursor-pointer items-center justify-center rounded-full bg-slate-950 py-4 text-lg font-medium text-white backdrop-blur-3xl">
+         Go To Dashboard 
+<ArrowRight className="ml-3"/>
+        </span>
+      </button>
     </div>
   );
 }
+
+export default Home
