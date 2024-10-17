f50464662cd91e41ca23a4d705e6f1d9e5274361
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-17 21:55:55 +0530

    testimonial schema

---
+++ src/models/Testimonial.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface TestimonialType extends Document {
+    user: mongoose.Types.ObjectId; // Reference to the User
+    name:string;
+    content: string;
+    rating: number; 
+    photo:string;
+
+}
+
+const testimonialSchema: Schema<TestimonialType> = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    name: {
+        type:String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        min: 1, // Assuming a rating system from 1 to 5
+        max: 5,
+    },
+}, {
+    timestamps: true,
+});
+
+const Testimonial = mongoose.models.Testimonial || mongoose.model<TestimonialType>('Testimonial', testimonialSchema);
+export default Testimonial;
