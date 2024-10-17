ef7918d59c5c3578328388c18dec8896a731be30
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-17 21:45:14 +0530

    user model

---
+++ src/models/User.ts
@@ -0,0 +1,38 @@
+import mongoose,{Document, Schema} from 'mongoose'
+
+interface userType extends Document {
+    name: string,
+    email: string,
+    password: string
+    limit:number
+    testimonials:mongoose.Types.ObjectId[]
+}
+
+const userSchema:Schema<userType>= new Schema({
+   name:{
+    type:String,
+    required:true
+   },
+   email:{
+    type:String,
+    required:true,
+   },
+   password:{
+    type:String,
+    required:true
+   },
+   limit:{
+    type:Number,
+    default:3
+   },
+   testimonials:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Testimonial'
+   }]
+    
+},{
+    timestamps:true
+})
+
+const User =mongoose.models.User || mongoose.model<userType>("User",userSchema)
+export default User;  
