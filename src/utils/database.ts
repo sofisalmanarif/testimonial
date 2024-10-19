6cac8814ac66ffa434353ca9149cd3c1b5b1a1db
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-19 19:43:40 +0530

    database file added

---
+++ src/utils/database.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+type Connection = {
+    isConnected?:number
+}
+
+const connection:Connection ={}
+
+const connectDatabase = async ()=>{
+    const url = process.env.MONGODB_URI || ""
+    try {
+        if(connection.isConnected){
+            console.log("DataBase is already connected")
+            return;
+    
+        }
+        const dbConnection = await mongoose.connect(url, {})
+        connection.isConnected = dbConnection.connections[0].readyState
+        console.log("DataBase is connected")
+    } catch (e) {
+        console.log(e)
+        console.log("DataBase connection Failed")
+    }
+}
+
+export default connectDatabase
