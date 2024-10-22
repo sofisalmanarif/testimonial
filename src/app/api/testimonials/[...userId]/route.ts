6963398b2d3fa0da4e6b29af8b2893abd65e16a7
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-22 21:03:36 +0530

    create testimonials api

---
+++ src/app/api/testimonials/[...userId]/route.ts
@@ -0,0 +1,36 @@
+import Testimonial from "@/models/Testimonial"
+import User from "@/models/User"
+import connectDatabase from "@/utils/database"
+
+export async function POST(req: Request, { params }: { params: { userId: string } }) {
+    await connectDatabase()
+
+    const {   name, content,rating,photo } = await req.json()
+    const { userId } = params
+
+    if(!name || !content  || !rating || !photo){
+        return Response.json({ success: false, message: "Enter All Fileds" }, { status: 400 })
+    }
+    const user = await User.findById(userId)
+    console.log(user)
+
+    if(user.limit<=0){
+        return Response.json({ success: false, message: "User Has Reached Free Limit" }, { status: 400 })
+    }
+    
+    if (!user) {
+        return Response.json({ success: false, message: "User Not Found" }, { status: 400 })
+    }
+    
+    const createdTestimonial = await Testimonial.create({
+        name,
+        content,
+        rating,
+        photo,
+        user:userId
+    })
+    user.limit = user.limit - 1 
+    await user.save()
+
+    return Response.json({ success:true,message: "Review Send", user ,createdTestimonial},{status:201})
+}
