8d86e7951df8990d607aa789e22157bb93fdac28
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-22 21:04:59 +0530

    error handelling in create testmonials api

--- src/app/api/testimonials/[...userId]/route.ts
+++ src/app/api/testimonials/[...userId]/route.ts
@@ -5,32 +5,37 @@ import connectDatabase from "@/utils/database"
 export async function POST(req: Request, { params }: { params: { userId: string } }) {
     await connectDatabase()
 
-    const {   name, content,rating,photo } = await req.json()
-    const { userId } = params
-
-    if(!name || !content  || !rating || !photo){
-        return Response.json({ success: false, message: "Enter All Fileds" }, { status: 400 })
-    }
-    const user = await User.findById(userId)
-    console.log(user)
-
-    if(user.limit<=0){
-        return Response.json({ success: false, message: "User Has Reached Free Limit" }, { status: 400 })
-    }
+    try {
+        const {   name, content,rating,photo } = await req.json()
+        const { userId } = params
     
-    if (!user) {
-        return Response.json({ success: false, message: "User Not Found" }, { status: 400 })
-    }
+        if(!name || !content  || !rating || !photo){
+            return Response.json({ success: false, message: "Enter All Fileds" }, { status: 400 })
+        }
+        const user = await User.findById(userId)
+        console.log(user)
     
-    const createdTestimonial = await Testimonial.create({
-        name,
-        content,
-        rating,
-        photo,
-        user:userId
-    })
-    user.limit = user.limit - 1 
-    await user.save()
-
-    return Response.json({ success:true,message: "Review Send", user ,createdTestimonial},{status:201})
+        if(user.limit<=0){
+            return Response.json({ success: false, message: "User Has Reached Free Limit" }, { status: 400 })
+        }
+        
+        if (!user) {
+            return Response.json({ success: false, message: "User Not Found" }, { status: 400 })
+        }
+        
+        const createdTestimonial = await Testimonial.create({
+            name,
+            content,
+            rating,
+            photo,
+            user:userId
+        })
+        user.limit = user.limit - 1 
+        await user.save()
+    
+        return Response.json({ success:true,message: "Review Send", user ,createdTestimonial},{status:201})
+    } catch (error) {
+        console.error('Error occurred:', error);
+        return Response.json({ success: false, message: "Internal Server Error" }, { status: 500 })
+    }
 }
